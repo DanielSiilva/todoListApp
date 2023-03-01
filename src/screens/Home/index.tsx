d57653716a040d7a7c0f0474c5052e56e9522476
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-28 21:58:15 -0300

    Feat: redefinição de pastas

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native'
+import { Header } from '../../components/Header'
+
+
+export function Home (){
+
+
+    return(
+        <View>
+            <Header />
+            <Text> Home</Text>
+        </View>
+    )
+}
