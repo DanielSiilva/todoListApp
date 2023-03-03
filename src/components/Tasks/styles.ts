fa26d5ff414bff21116660c4868bf443b37b2c25
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-03 09:06:27 -0300

    Styled: centralizando itens do card de tacks

--- src/components/Tasks/styles.ts
+++ src/components/Tasks/styles.ts
@@ -3,6 +3,8 @@ import styled from "styled-components/native";
 
 export const Card = styled.View`
     flex-direction: row;
+    align-items: center;
+    justify-content: center;
 
     padding: 12px 8px 12px 12px;
     gap: 8px;
@@ -21,8 +23,7 @@ export const Card = styled.View`
 
 export const TextTask = styled.Text`
     width: 300px;
-    height: 40px;
-
+    
     color: ${props => props.theme['gray_100']};
     font-weight: 400;
     font-size: 14px; 
